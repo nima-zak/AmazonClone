f19a65eaae5981893c7365e17228be8b6a549750
nima-zak <z.nima27@gmail.com>
2022-04-10 21:51:13 -0500

    use React Router and make 3 component

--- amazon-ui/src/App.js
+++ amazon-ui/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Checkout from "./component/Checkout";
+import Home from "./component/Home.js";
+import Login from "./component/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/checkout" element={<Checkout />}></Route>
+            <Route path="login" element={<Login />}></Route>
+          
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ amazon-ui/src/component/Checkout.js
@@ -0,0 +1,5 @@
+import React from "react";
+function  Checkout() { 
+    return <h1>Checkout</h1>
+ }
+ export default Checkout;
